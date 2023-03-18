07aa7b47dbfa746a687030d5a76bfeb66dc90ce0
Alper Doğan <alperuni217@gmail.com>
2023-03-19 01:27:26 +0300

    feat(wip): start implementing blog main page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
-import { defineConfig } from 'astro/config'
-import vue from '@astrojs/vue'
-import tailwind from '@astrojs/tailwind'
-import image from '@astrojs/image'
+import { defineConfig } from 'astro/config';
+import vue from '@astrojs/vue';
+import tailwind from '@astrojs/tailwind';
+import image from '@astrojs/image';
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [vue(), tailwind(), image()],
-})
+  integrations: [vue(), tailwind(), image(), mdx()]
+});
