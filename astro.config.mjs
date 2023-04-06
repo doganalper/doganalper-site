07236b8feba02693b14845a252cae23b24ab7b2c
Alper Doğan <alperuni217@gmail.com>
2023-04-06 10:53:53 +0300

    feat: add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,15 @@ import { defineConfig } from 'astro/config';
 import vue from '@astrojs/vue';
 import tailwind from '@astrojs/tailwind';
 import image from '@astrojs/image';
-
 import mdx from "@astrojs/mdx";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue(), tailwind(), image(), mdx()]
+  integrations: [vue(), tailwind(), image(), mdx()],
+  output: "server",
+	adapter: vercel({
+		analytics: true
+	})
 });
