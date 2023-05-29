d967918357317fd10d6f91a12c47b7a6eebe7ef5
Alper Doğan <alperuni217@gmail.com>
2023-05-29 22:06:36 +0300

    feat: add vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,14 @@
-import { defineConfig } from 'astro/config';
-import vue from '@astrojs/vue';
-import tailwind from '@astrojs/tailwind';
-import mdx from "@astrojs/mdx";
-
-// import vercel from "@astrojs/vercel/serverless";
+import { defineConfig } from 'astro/config'
+import vue from '@astrojs/vue'
+import tailwind from '@astrojs/tailwind'
+import mdx from '@astrojs/mdx'
+import vercel from '@astrojs/vercel/serverless'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue(), tailwind(), mdx()],
-  // output: "server",
-	// adapter: vercel({
-	// 	analytics: true
-	// })
-});
+	integrations: [vue(), tailwind(), mdx()],
+	output: 'server',
+	adapter: vercel({
+		analytics: true,
+	}),
+})
