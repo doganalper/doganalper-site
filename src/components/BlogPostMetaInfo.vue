b0e876f442815f28c375549f7998cc4bb993c1cc
Alper Doğan <alperuni217@gmail.com>
2023-05-29 18:05:48 +0300

    feat: add locales to tags

--- src/components/BlogPostMetaInfo.vue
+++ src/components/BlogPostMetaInfo.vue
@@ -2,23 +2,45 @@
 import type { CollectionEntry } from 'astro:content'
 import capitalizeString from 'src/utils/capitalizeString'
 import formatDate from 'src/utils/formatDate'
+import { computed } from 'vue'
 
 const props = defineProps<{
-	entry: CollectionEntry<any>
+	entry: CollectionEntry<'blog'>
 }>()
 
 const publishDate = formatDate(props.entry.data.publishDate, {
 	locale: props.entry.data.language || 'en',
 	weekday: false
 })
+
+const tags = {
+	tr: {
+		'development': "Yazılım",
+		'travel': "Gezi",
+		'life': 'Hayat'
+	},
+	en: {
+		'development': "Development",
+		'travel': "Travel",
+		'life': 'Life'
+	}
+}
+
+const tag = computed(() => {
+	return tags[props.entry.data.language || 'en'][props.entry.data.tag]
+})
+
+const time = computed(() => {
+	return props.entry.data.publishDate.toISOString()
+})
 </script>
 
 <template>
 	<div class="space-x-2.5 text-sm">
-		<time :datetime="entry.data.publishDate">
+		<time :datetime="time">
 			{{ publishDate }}
 		</time>
 		<span>-</span>
-		<span>{{ capitalizeString(entry.data.tag) }}</span>
+		<span>{{tag}}</span>
 	</div>
 </template>
