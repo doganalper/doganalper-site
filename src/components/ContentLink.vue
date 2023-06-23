7b52ac0db0d890cf779c87cb42c6d42e9a936fe8
Alper Doğan <alperuni217@gmail.com>
2023-06-23 10:26:51 +0300

    blog: create a new blog post called work-setup

---
+++ src/components/ContentLink.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+defineProps<{
+	href: string
+	text: string
+	isOutLink: boolean
+}>()
+</script>
+
+<template>
+	<a
+		:href="href"
+		:target="isOutLink ? '_blank' : '_self'"
+		class="underline underline-offset-1 dark:hover:text-white hover:text-black"
+	>
+		{{ text }}
+	</a>
+</template>
