8818681e45344f3d41621111aa900c48eb51e66d
Alper Doğan <alperuni217@gmail.com>
2023-08-13 01:48:04 +0300

    chore(og-image): add prerender to try

--- src/pages/blog/[tag]/[...slug]/og-image.png.ts
+++ src/pages/blog/[tag]/[...slug]/og-image.png.ts
@@ -2,6 +2,8 @@ import type { APIRoute } from 'astro'
 import { getCollection, getEntryBySlug } from 'astro:content'
 import renderOgImage from 'src/utils/renderOgImage'
 
+export const prerender = true
+
 type Slugs = Awaited<ReturnType<typeof getCollection>>[0]['slug']
 export async function getStaticPath() {
 	const posts = await getCollection('blog')
