5e17a670f29a76fa96e7bab31b6c2d44b166feff
Alper Doğan <alperuni217@gmail.com>
2023-08-15 21:07:45 +0300

    feat(blog): implement reading time

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,14 @@ import tailwind from '@astrojs/tailwind'
 import mdx from '@astrojs/mdx'
 import vercel from '@astrojs/vercel/serverless'
 import sitemap from '@astrojs/sitemap'
+import { remarkReadingTime } from './reading-time.mjs'
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [vue(), tailwind(), mdx(), sitemap()],
+	markdown: {
+		remarkPlugins: [remarkReadingTime]
+	},
 	output: 'hybrid',
 	adapter: vercel({
 		analytics: true,
