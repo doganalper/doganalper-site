1e7cf5932e57cc6fb63be8b4df5563e48fccbdd9
Alper Doğan <alperuni217@gmail.com>
2023-10-04 17:17:26 +0300

    book: add social justice and the city

--- src/constants/book-log.ts
+++ src/constants/book-log.ts
@@ -7,10 +7,16 @@ type BookLog = {
 
 const bookLog: BookLog[] = [
 	{
+		name: 'Social Justice and the City',
+		author: 'David Harvey',
+		current: true,
+		link: 'https://www.goodreads.com/book/show/184940.Social_Justice_and_the_City',
+	},
+	{
 		name: 'Chronicle of a Death Foretold',
 		author: 'Gabriel García Márquez',
 		current: false,
-		link: 'https://www.goodreads.com/book/show/23878.Chronicle_of_a_Death_Foretold?ac=1&from_search=true&qid=whXe70ulYR&rank=1',
+		link: 'https://www.goodreads.com/book/show/23878.Chronicle_of_a_Death_Foretold',
 	},
 ]
 
