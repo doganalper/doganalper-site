2e2d727cf0035c31ad8c452d9591e6422c9f5642
Alper Doğan <alperuni217@gmail.com>
2023-11-10 21:01:02 +0300

    fix(general): ExternalLink not displaying properly, add Akinon

--- src/types/job.ts
+++ src/types/job.ts
@@ -2,7 +2,7 @@ export interface IExperience {
 	company: string
 	role: string
 	href: string
-	companySummary: string
+	companySummary?: string
 	roleSummary: string[]
 	stack: string[]
 	dates: string[]
