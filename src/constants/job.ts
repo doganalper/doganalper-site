5d5a94c3ae04f79e0659b37204b890b5828c46a8
Alper Doğan <alperuni217@gmail.com>
2023-12-02 00:42:22 +0300

    chore(job): add end date for akinon

--- src/constants/job.ts
+++ src/constants/job.ts
@@ -1,61 +1,61 @@
 const Jobs = [
-	{
-		company: 'Akinon',
-		role: 'Frontend Developer',
-		href: 'https://akinon.com/',
-		roleSummary: [
-			'🚧 Developing and maintaining e-commerce websites using Jinja and Javascript.',
-			"Implementing design changes and creating new pages depending on customer's requests.",
-		],
-		stack: ['Javascript', 'Tailwindcss', 'Scss', 'Django Jinja'],
-		dates: ['10/2021'],
-	},
-	{
-		company: 'StoreSpy',
-		role: 'Frontend Developer',
-		href: 'https://storespy.net/',
-		companySummary:
-			'StoreSpy allows you to reach the data and analytics to build the best-optimized mobile apps by tracking their performance.',
-		roleSummary: [
-			'🚧 Developing and maintaining web app using NuxtJs, Javascript, Vuetify',
-			'🧪 Unit tests with Jest and E2E tests with Cypress',
-			'🛫 Migrated one of our projects to Typescript',
-		],
-		stack: [
-			'Javascript',
-			'Typescript',
-			'NuxtJs',
-			'VueJs',
-			'Tailwindcss',
-			'Astro',
-		],
-		dates: ['09/2021', '05/2023'],
-	},
-	{
-		company: 'Nerasocial',
-		role: 'Junior Frontend Developer',
-		href: 'https://www.nerasocial.com/',
-		companySummary:
-			'Manage your social media accounts with two clicks! Nera designs for you, writes captions.Stop thinking about content for social media posts.',
-		roleSummary: [
-			'Developed web app using ReactJs with Javascript and Tailwindcss',
-			'Created a library for canvas to add new images, objects and text to the canvas and also make these elements moveable, resizable and rotatable',
-		],
-		stack: ['Javascript', 'Tailwindcss', 'ReactJs'],
-		dates: ['11/2020', '07/2021'],
-	},
-	{
-		company: 'Scoutium',
-		role: 'Frontend Developer Intern',
-		href: 'https://scoutium.com/',
-		companySummary:
-			'Scoutium is a technology company that provides a unique motivation for all players, clubs and federations around the world. With dozens of partner clubs, Scoutium is able to access hundreds of cities, towns, and villages with hundreds of video experts and scouts. We aim to democratise football by opening up young players’ horizons and giving opportunities to players to be discovered.',
-		roleSummary: [
-			'Developed web app using VueJs with Javascript and Tailwindcss',
-		],
-		stack: ['Javascript', 'Tailwindcss', 'VueJs', 'Scss'],
-		dates: ['09/2020', '11/2020'],
-	},
+  {
+    company: 'Akinon',
+    role: 'Frontend Developer',
+    href: 'https://akinon.com/',
+    roleSummary: [
+      '🚧 Developing and maintaining e-commerce websites using Jinja and Javascript.',
+      "Implementing design changes and creating new pages depending on customer's requests.",
+    ],
+    stack: ['Javascript', 'Tailwindcss', 'Scss', 'Django Jinja'],
+    dates: ['10/2021', '11/2021'],
+  },
+  {
+    company: 'StoreSpy',
+    role: 'Frontend Developer',
+    href: 'https://storespy.net/',
+    companySummary:
+    'StoreSpy allows you to reach the data and analytics to build the best-optimized mobile apps by tracking their performance.',
+    roleSummary: [
+      '🚧 Developing and maintaining web app using NuxtJs, Javascript, Vuetify',
+      '🧪 Unit tests with Jest and E2E tests with Cypress',
+      '🛫 Migrated one of our projects to Typescript',
+    ],
+    stack: [
+      'Javascript',
+      'Typescript',
+      'NuxtJs',
+      'VueJs',
+      'Tailwindcss',
+      'Astro',
+    ],
+    dates: ['09/2021', '05/2023'],
+  },
+  {
+    company: 'Nerasocial',
+    role: 'Junior Frontend Developer',
+    href: 'https://www.nerasocial.com/',
+    companySummary:
+    'Manage your social media accounts with two clicks! Nera designs for you, writes captions.Stop thinking about content for social media posts.',
+    roleSummary: [
+      'Developed web app using ReactJs with Javascript and Tailwindcss',
+      'Created a library for canvas to add new images, objects and text to the canvas and also make these elements moveable, resizable and rotatable',
+    ],
+    stack: ['Javascript', 'Tailwindcss', 'ReactJs'],
+    dates: ['11/2020', '07/2021'],
+  },
+  {
+    company: 'Scoutium',
+    role: 'Frontend Developer Intern',
+    href: 'https://scoutium.com/',
+    companySummary:
+    'Scoutium is a technology company that provides a unique motivation for all players, clubs and federations around the world. With dozens of partner clubs, Scoutium is able to access hundreds of cities, towns, and villages with hundreds of video experts and scouts. We aim to democratise football by opening up young players’ horizons and giving opportunities to players to be discovered.',
+    roleSummary: [
+      'Developed web app using VueJs with Javascript and Tailwindcss',
+    ],
+    stack: ['Javascript', 'Tailwindcss', 'VueJs', 'Scss'],
+    dates: ['09/2020', '11/2020'],
+  },
 ]
 
 export default Jobs
