6725ebd89a32e34194cda158300330ddf7e4f2ac
Alper Doğan <alperuni217@gmail.com>
2024-02-06 11:34:17 +0300

    feat: add prerender to default og image

--- src/pages/default-meta-image.png.ts
+++ src/pages/default-meta-image.png.ts
@@ -1,6 +1,8 @@
 import type { APIRoute } from "astro";
 import renderOgImage from "src/utils/renderOgImage";
 
+export const prerender = true;
+
 export const GET: APIRoute = async function GET() {
   return renderOgImage("Alper Doğan", ".dev");
 };
